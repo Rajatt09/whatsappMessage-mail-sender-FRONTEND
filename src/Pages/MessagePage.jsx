5669f31aac93d9268dd5d8ab1b25d8bde8f00e3c
Yash Mittal <mittalyas1234@gmail.com>
2024-08-26 23:48:23 +0530

    sending msg

--- src/Pages/MessagePage.jsx
+++ src/Pages/MessagePage.jsx
@@ -5,44 +5,26 @@ import {
   Col,
   Button,
   Table,
-  // Card,
   Tabs,
   Tab,
-  Modal
+  Modal,
+  Card,
 } from "react-bootstrap";
 import { ClipLoader } from "react-spinners";
+import { CheckCircleFill, XCircleFill } from 'react-bootstrap-icons';
 import "./MessagePage.css";
-import { CheckCircleFill } from 'react-bootstrap-icons';
-
-import { XCircleFill } from 'react-bootstrap-icons';
-
 
 const MessagePage = () => {
   const [fileData, setFileData] = useState([]);
   const [activeTab, setActiveTab] = useState("message");
-  // const [uploaded, setUploaded] = useState(false);
   const [showError, setShowError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [status, setStatus] = useState([]);
-  const [showModal, setShowModal] = useState(false); // State to control modal visibility
-
-  // const handleFileUpload = (event) => {
-  //   const file = event.target.files[0];
-  //   if (file) {
-  //     setUploaded(true);
-  //     setFileData(file);
-  //     // setshowError(false);
-  //     // Parsing logic can be added here if necessary (e.g., using a library like papaparse)
-  //   } else {
-  //     setUploaded(false);
-  //     setFileData([]);
-  //   }
-  // };
+  const [showModal, setShowModal] = useState(false);
 
   const sendEmails = () => {
     setLoading(true);
-    setActiveTab("sent-status"); // Switch to "Sent Status" tab
-
+    setActiveTab("sent-status");
 
     setFileData((prevFileData) =>
       prevFileData.map(user => ({ ...user, emailStatus: "sending" }))
@@ -86,7 +68,6 @@ const MessagePage = () => {
               email: update.email,
               emailSent: update.emailSent === "yes",
               emailStatus: update.emailSent,
-              messageSent: false, // Update this based on your logic for WhatsApp messages
             });
           }
 
@@ -105,6 +86,204 @@ const MessagePage = () => {
       eventSource.close();
     };
   };
+
+  const sendWhatsAppMessages = () => {
+    setLoading(true);
+    setActiveTab("sent-status");
+
+    setFileData((prevFileData) =>
+      prevFileData.map(user => ({ ...user, messageStatus: "sending" }))
+    );
+
+    const eventSource = new EventSource(
+      "http://localhost:8000/api/v1/message/sendMessage/via-whatsapp"
+    );
+
+    setFileData((prevFileData) =>
+      prevFileData.map(user => ({ ...user, messageStatus: "sending" }))
+    );
+    eventSource.onopen = () => {
+      console.log("Connection to server opened.");
+    };
+
+    eventSource.onmessage = (event) => {
+      console.log("EventSource message:", event.data);
+      setStatus((prevStatus) => [...prevStatus, event.data]);
+
+      const update = JSON.parse(event.data);
+      console.log("Update:", update);
+
+      if (update.message === "All messages processed") {
+        setLoading(false);
+        eventSource.close();
+        setShowModal(true);
+      } else {
+        setFileData((prevFileData) => {
+          const updatedFileData = prevFileData.map((user) =>
+            user.phoneNo === update.phoneNo
+              ? {
+                ...user,
+                messageSent: update.messageSent === "yes",
+                messageStatus: update.messageSent,
+              }
+              : user
+          );
+
+          if (update.phoneNo && !updatedFileData.find((user) => user.phoneNo === update.phoneNo)) {
+            updatedFileData.push({
+              name: update.name,
+              phoneNo: update.phoneNo,
+              email: update.email,
+              messageSent: update.messageSent === "yes",
+              messageStatus: update.messageSent,
+            });
+          }
+
+          return updatedFileData;
+        });
+      }
+    };
+
+    eventSource.onerror = (error) => {
+      console.error("EventSource failed:", error);
+      setStatus((prevStatus) => [
+        ...prevStatus,
+        "An error occurred while sending WhatsApp messages.",
+      ]);
+      setLoading(false);
+      eventSource.close();
+    };
+  };
+
+
+  // const sendBoth = () => {
+  //   setLoading(true);
+  //   setActiveTab("sent-status");
+
+  //   setFileData((prevFileData) =>
+  //     prevFileData.map((user) => ({
+  //       ...user,
+  //       emailStatus: "sending",
+  //       messageStatus: "sending",
+  //     }))
+  //   );
+
+  //   const eventSource = new EventSource(
+  //     "http://localhost:8000/api/v1/message/sendMesage/via-both"
+  //   );
+
+  //   eventSource.onopen = () => {
+  //     console.log("Connection to server opened.");
+  //   };
+
+  //   eventSource.onmessage = (event) => {
+  //     console.log("EventSource message:", event.data);
+  //     setStatus((prevStatus) => [...prevStatus, event.data]);
+
+  //     const update = JSON.parse(event.data);
+  //     console.log("Update:", update);
+
+  //     if (update.message === "All messages processed") {
+  //       setLoading(false);
+  //       eventSource.close();
+  //       setShowModal(true);
+  //     } else {
+  //       setFileData((prevFileData) => {
+  //         const updatedFileData = prevFileData.map((user) => {
+  //           if (user.email === update.email || user.phoneNo === update.phoneNo) {
+  //             return {
+  //               ...user,
+  //               emailSent: update.emailSent === "yes",
+  //               emailStatus: update.emailSent || user.emailStatus,
+  //               messageSent: update.messageSent === "yes",
+  //               messageStatus: update.messageSent || user.messageStatus,
+  //             };
+  //           }
+  //           return user;
+  //         });
+
+  //         if (update.phoneNo && !updatedFileData.find((user) => user.phoneNo === update.phoneNo)) {
+  //           updatedFileData.push({
+  //             name: update.name,
+  //             phoneNo: update.phoneNo,
+  //             email: update.email,
+  //             emailSent: update.emailSent === "yes",
+  //             emailStatus: update.emailSent,
+  //             messageSent: update.messageSent === "yes",
+  //             messageStatus: update.messageSent,
+  //           });
+  //         }
+
+  //         return updatedFileData;
+  //       });
+  //     }
+  //   };
+
+  //   eventSource.onerror = (error) => {
+  //     console.error("EventSource failed:", error);
+  //     setStatus((prevStatus) => [
+  //       ...prevStatus,
+  //       "An error occurred while sending both emails and WhatsApp messages.",
+  //     ]);
+  //     setLoading(false);
+  //     eventSource.close();
+  //   };
+  // };
+
+  // uploading a excel file
+  const uploadFile = async (e) => {
+    // checking if file only is excel file
+
+    const file = e.target.files[0];
+
+
+    if (file.name.split('.').pop() !== 'xlsx') {
+      alert('Please upload a valid excel file');
+      setFileData([]);
+
+      // clear form data
+      e.target
+        ? e.target.value = ''
+        : null;
+
+
+
+      return;
+    }
+
+
+
+    const formData = new FormData();
+
+    if (!file) {
+      setShowError(true);
+      return;
+    }
+    setShowError(false);
+
+
+    formData.append("file", file);
+
+    try {
+      const response = await fetch("http://localhost:8000/api/v1/message/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setFileData(data);
+      } else {
+        console.error("Failed to upload file");
+      }
+    } catch (error) {
+      console.error("Failed to upload file", error);
+    }
+  };
+
+
+
+
 
   const viewDetails = () => {
     if (!fileData.length) {
@@ -157,6 +336,22 @@ const MessagePage = () => {
                     </Button>
                     <Button
                       variant="info"
+                      onClick={sendWhatsAppMessages}
+                      className="m-2"
+                      disabled={loading}
+                    >
+                      Send WhatsApp Messages
+                    </Button>
+                    {/* <Button
+                      variant="success"
+                      onClick={sendBoth}
+                      className="m-2"
+                      disabled={loading}
+                    >
+                      Send Both
+                    </Button> */}
+                    <Button
+                      variant="info"
                       onClick={additionalAction}
                       className="m-2"
                       disabled={loading}
@@ -165,16 +360,9 @@ const MessagePage = () => {
                     </Button>
                   </Col>
                 </Row>
-                {/* <Row className="justify-content-center">
-                  <Col md={8}>
-                    <Card className="p-4 mb-4 shadow">
-                     
-                    </Card>
-                  </Col>
-                </Row> */}
                 {loading && (
                   <div className="text-center">
-                    {/* <ClipLoader size={35} color={"#123abc"} /> */}
+                    <ClipLoader size={35} color={"#123abc"} />
                   </div>
                 )}
                 <div>
@@ -183,6 +371,7 @@ const MessagePage = () => {
                   ))}
                 </div>
               </Tab>
+
               <Tab eventKey="sent-status" title="Sent Status">
                 <Row className="justify-content-center">
                   <Col md={12} style={{ textAlign: "center" }}>
@@ -207,14 +396,20 @@ const MessagePage = () => {
                                 {user.emailStatus === "sending" ? (
                                   <ClipLoader size={20} color={"#123abc"} />
                                 ) : user.emailStatus === "yes" ? (
-                                  // green  tick icon
                                   <CheckCircleFill color="green" />
                                 ) : (
-                                  // red cross icon
                                   <XCircleFill color="red" />
                                 )}
                               </td>
-                              <td>{user.messageSent ? "Yes" : "No"}</td>
+                              <td>
+                                {user.messageStatus === "sending" ? (
+                                  <ClipLoader size={20} color={"#123abc"} />
+                                ) : user.messageStatus === "yes" ? (
+                                  <CheckCircleFill color="green" />
+                                ) : (
+                                  <XCircleFill color="red" />
+                                )}
+                              </td>
                             </tr>
                           ))
                         ) : (
@@ -232,23 +427,55 @@ const MessagePage = () => {
             </Tabs>
           </Col>
         </Row>
-        {/* <Row className="justify-content-center">
-          <Col md={8}>
-            <Card className="p-4 mb-4 shadow">
-              <Card.Body>
-                <h3>Upload Excel File</h3>
-                <input
-                  type="file"
-                  onChange={handleFileUpload}
-                  accept=".xlsx, .xls, .csv"
-                />
-                {showError && (
-                  <p className="text-danger mt-2">No file chosen</p>
-                )}
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row> */}
+
+
+
+
+        {
+          activeTab === "message" && (
+            <Row className="justify-content-center">
+              <Col md={8}>
+                <Card className="p-4 mb-4 shadow">
+                  <Card.Body>
+                    <h3>Upload Excel File</h3>
+                    <input
+                      type="file"
+                      className="form-control"
+                      onChange={uploadFile}
+                      accept=".xlsx, .xls, .csv"
+                    />
+                    {showError && (
+                      <p className="text-danger mt-2">No file chosen</p>
+                      // add button for uploading file
+
+
+                    )}
+
+
+                    {
+
+                      // button for uploading file
+                      <Button
+                        variant="primary"
+                        className="mt-3"
+                        onClick={viewDetails}
+                      >
+                        Upload File
+                      </Button>
+
+                    }
+
+                    <p className="mt-3">
+                      <strong>Instructions: File only in excel format </strong>
+                    </p>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          )
+        }
+
+
       </main>
 
       <Modal show={showModal} onHide={() => setShowModal(false)}>
@@ -256,7 +483,7 @@ const MessagePage = () => {
           <Modal.Title>Emails Sent</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          All emails have been successfully sent!
+          All emails and WhatsApp messages have been successfully sent!
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => setShowModal(false)}>
